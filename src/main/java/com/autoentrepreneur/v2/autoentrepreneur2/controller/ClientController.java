6bef08d30e4cdd3cf15ef1e9c446006e93394f45
Ems <guesbaya@hotmail.fr>
2022-03-08 16:08:12 +0100

    delete api works

--- src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/controller/ClientController.java
+++ src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/controller/ClientController.java
@@ -8,8 +8,11 @@ import com.autoentrepreneur.v2.autoentrepreneur2.model.Client;
 import com.autoentrepreneur.v2.autoentrepreneur2.repository.ClientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,6 +60,12 @@ public class ClientController {
     @GetMapping("/dateMAJ/{dateMAJ}")
     public List<Client> getClientBydateMAJ(@PathVariable LocalDateTime dateMAJ) {
         return clientRepository.findByDateMAJ(dateMAJ);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteClientById(@PathVariable Long id){
+        clientRepository.deleteById(id);
+        return ResponseEntity.ok("Client id#" + id + " has been deleted.");
     }
     
 }
