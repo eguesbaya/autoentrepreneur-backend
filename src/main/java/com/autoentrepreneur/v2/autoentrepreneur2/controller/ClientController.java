14cc6fd77a66121277d2c27fea64833f2262b223
Ems <guesbaya@hotmail.fr>
2022-03-08 15:53:03 +0100

    Read, sorted by each field

--- src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/controller/ClientController.java
+++ src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/controller/ClientController.java
@@ -1,5 +1,6 @@
 package com.autoentrepreneur.v2.autoentrepreneur2.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.autoentrepreneur.v2.autoentrepreneur2.exception.ResourceNotFoundException;
@@ -19,12 +20,11 @@ public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
 
-    @GetMapping("/all")
+    @GetMapping("/")
     public List<Client> getClients() {
         return clientRepository.findAll();
     }
 
-    // get contact by ID
     @GetMapping("/{id}")
     public Client getClient(@PathVariable Long id) {
         if (clientRepository.findById(id).isPresent()) {
@@ -32,6 +32,31 @@ public class ClientController {
         } else {
             throw new ResourceNotFoundException("Client id#" + id + " n'existe pas.");
         }
+    }
+
+    @GetMapping("/siret/{siret}")
+    public List<Client> getClientBySiret(@PathVariable String siret) {
+        return clientRepository.findBySiret(siret);
+    }
+
+    @GetMapping("/siren/{siren}")
+    public List<Client> getClientBySiren(@PathVariable String siren) {
+        return clientRepository.findBySiren(siren);
+    }
+
+    @GetMapping("/nom/{nomRaisonSociale}")
+    public List<Client> getClientByNomRaisonSociale(@PathVariable String nomRaisonSociale) {
+        return clientRepository.findByNomRaisonSociale(nomRaisonSociale);
+    }
+
+    @GetMapping("/dateCreation/{dateCreation}")
+    public List<Client> getClientByDateCreation(@PathVariable LocalDateTime dateCreation) {
+        return clientRepository.findByDateCreation(dateCreation);
+    }
+
+    @GetMapping("/dateMAJ/{dateMAJ}")
+    public List<Client> getClientBydateMAJ(@PathVariable LocalDateTime dateMAJ) {
+        return clientRepository.findByDateMAJ(dateMAJ);
     }
     
 }
--- src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/repository/ClientRepository.java
+++ src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/repository/ClientRepository.java
@@ -1,5 +1,8 @@
 package com.autoentrepreneur.v2.autoentrepreneur2.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import com.autoentrepreneur.v2.autoentrepreneur2.model.Client;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +10,16 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long>{
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    
+    List<Client> findBySiret(String siret);
+
+    List<Client> findByDateCreation(LocalDateTime dateCreation);
+
+    List<Client> findByDateMAJ(LocalDateTime dateMAJ);
+    
+    List<Client> findByNomRaisonSociale(String nomRaisonSociale);
+
+    List<Client> findBySiren(String siren);
     
 }
