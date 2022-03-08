5babf5e51979da1169d42ccc984f31380f0b6d37
Ems <guesbaya@hotmail.fr>
2022-03-08 16:27:47 +0100

    cleaned warnings

--- src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/model/Contact.java
+++ src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/model/Contact.java
@@ -11,13 +11,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.annotation.Transient;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
