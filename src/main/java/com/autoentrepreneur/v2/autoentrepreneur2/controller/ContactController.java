820cde2594acc2fe5c2055cf7be788a423d472e9
eguesbaya <guesbaya@hotmail.fr>
2022-03-10 16:43:22 +0100

    Read: all and get by each field

--- src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/controller/ContactController.java
+++ src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/controller/ContactController.java
@@ -1,5 +1,6 @@
 package com.autoentrepreneur.v2.autoentrepreneur2.controller;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import com.autoentrepreneur.v2.autoentrepreneur2.dto.ContactDTO;
@@ -27,8 +28,52 @@ public class ContactController {
     }
 
     @GetMapping("/{id}")
-    public ContactDTO getClient(@PathVariable Long id) {
+    public ContactDTO getById(@PathVariable Long id) {
         return contactService.getById(id);
     }
 
+    @GetMapping("/client/{id}")
+    public List<ContactDTO> getByClient(@PathVariable Long id) {
+        return contactService.getByClient(id);
+    }
+
+    @GetMapping("/email/{email}")
+    public List<ContactDTO> getByEmail(@PathVariable String email) {
+        return contactService.getByEmail(email);
+    }
+
+    @GetMapping("/prenom/{prenom}")
+    public List<ContactDTO> getByPrenom(@PathVariable String prenom) {
+        return contactService.getByPrenom(prenom);
+    }
+
+    @GetMapping("/nom/{nom}")
+    public List<ContactDTO> getByNom(@PathVariable String nom) {
+        return contactService.getByNom(nom);
+    }
+
+    @GetMapping("/principal/{isContactPrincipal}")
+    public List<ContactDTO> getByContactPrincipal(@PathVariable Boolean isContactPrincipal) {
+        return contactService.getByContactPrincipal(isContactPrincipal);
+    }
+
+    @GetMapping("/mobile/{mobile}")
+    public List<ContactDTO> getByMobile(@PathVariable String mobile) {
+        return contactService.getByMobile(mobile);
+    }
+
+    @GetMapping("/telephone/{telephone}")
+    public List<ContactDTO> getByTelephone(@PathVariable String telephone) {
+        return contactService.getByMobile(telephone);
+    }
+
+    @GetMapping("/dateCreation/{dateCreation}")
+    public List<ContactDTO> getByTelephone(@PathVariable Timestamp dateCreation) {
+        return contactService.getByDateCreation(dateCreation);
+    }
+
+    @GetMapping("/dateMAJ/{dateMAJ}")
+    public List<ContactDTO> getByDateMAJ(@PathVariable Timestamp dateMAJ) {
+        return contactService.getByDateMAJ(dateMAJ);
+    }
 }
--- src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/service/ContactService.java
+++ src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/service/ContactService.java
@@ -2,6 +2,7 @@ package com.autoentrepreneur.v2.autoentrepreneur2.service;
 
 import com.autoentrepreneur.v2.autoentrepreneur2.repository.ContactRepository;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,6 +11,7 @@ import com.autoentrepreneur.v2.autoentrepreneur2.model.Contact;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ContactService {
@@ -28,6 +30,70 @@ public class ContactService {
         return contactRepository.findById(id).get().convertToDTO();
     }
 
+    public List<ContactDTO> getByClient(Long clientId) {
+        return contactRepository.findByClient(clientId)
+                .stream()
+                .map(Contact::convertToDTO)
+                .collect(Collectors.toList());
+    }
     
+    public List<ContactDTO> getByEmail(String email) {
+        return contactRepository.findByEmail(email)
+                .stream()
+                .map(Contact::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ContactDTO> getByPrenom(String prenom) {
+        return contactRepository.findByPrenom(prenom)
+                .stream()
+                .map(Contact::convertToDTO)
+                .collect(Collectors.toList());
+    }
+    
+    public List<ContactDTO> getByNom(String nom) {
+        return contactRepository.findByNom(nom)
+                .stream()
+                .map(Contact::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ContactDTO> getByDateCreation(Timestamp dateCreation) {
+        return contactRepository.findByDateCreation(dateCreation)
+                .stream()
+                .map(Contact::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ContactDTO> getByDateMAJ(Timestamp dateMAJ) {
+        return contactRepository.findByDateMAJ(dateMAJ)
+                .stream()
+                .map(Contact::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+
+    // List<Contact> findByDateMAJ(Timestamp dateMAJ);
+
+    public List<ContactDTO> getByContactPrincipal(Boolean isContactPrincipal) {
+        return contactRepository.findByIsContactPrincipal(isContactPrincipal)
+                .stream()
+                .map(Contact::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ContactDTO> getByMobile(String mobile) {
+        return contactRepository.findByMobile(mobile)
+                .stream()
+                .map(Contact::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ContactDTO> getByTelephone(String telephone) {
+        return contactRepository.findByTelephone(telephone)
+                .stream()
+                .map(Contact::convertToDTO)
+                .collect(Collectors.toList());
+    }
     
 }
