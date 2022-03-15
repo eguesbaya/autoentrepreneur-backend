ebd31d886510de9824520937032c553316b6ca2b
Ems <guesbaya@hotmail.fr>
2022-03-15 12:16:38 +0100

    allows cross origin

--- src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/controller/ClientController.java
+++ src/main/java/com/autoentrepreneur/v2/autoentrepreneur2/controller/ClientController.java
@@ -11,6 +11,7 @@ import com.autoentrepreneur.v2.autoentrepreneur2.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/clients")
+@CrossOrigin
 public class ClientController {
 
     @Autowired
